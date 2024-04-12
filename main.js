4e2a3e4b956950e1e0ac54c9e3403e666eaf0fb7
Denis Petukhov <d.petukhov@skbkontur.ru>
2024-04-12 23:42:50 +0300

    Fix statements arrangement

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const toKontur = toArrow + kontur;
 const toTestKontur = toArrow + testKontur;
 
 const currentUrl = window.location.toString();
-const button = document.createElement("button");
 const isTestKontur = currentUrl.includes(testKontur);
+
+const button = document.createElement("button");
 button.textContent = isTestKontur ? toKontur : toTestKontur;
 button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none;color:rgb(102, 204, 255)"
-document.body.appendChild(button);
-
-button.addEventListener('click', () => {    
+button.addEventListener('click', () => {
     const newUrl = isTestKontur ? currentUrl.replace(testKontur, kontur) : currentUrl.replace(kontur, testKontur);
     window.location.href = newUrl;
 });
+
+document.body.appendChild(button);
