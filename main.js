6da3fa8ae0d7e958a2b2378ba294caae1845087f
Denis Petukhov <d.petukhov@skbkontur.ru>
2024-04-13 00:16:57 +0300

    Fix bug

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ const toArrow = "→ ";
 const toKontur = toArrow + kontur;
 const toTestKontur = toArrow + testKontur;
 
-const currentUrl = window.location.toString();
-const isTestKontur = currentUrl.includes(testKontur);
+function getLocationData() {
+    const location = window.location.toString();
+    const isTestKontur = location.includes(testKontur);
+    return { location, isTestKontur };
+};
 
 const button = document.createElement("button");
-button.textContent = isTestKontur ? toKontur : toTestKontur;
+button.textContent = getLocationData().isTestKontur ? toKontur : toTestKontur;
 button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none;color:rgb(102, 204, 255)"
+
 button.addEventListener('click', () => {
-    const newUrl = isTestKontur ? currentUrl.replace(testKontur, kontur) : currentUrl.replace(kontur, testKontur);
-    window.location.href = newUrl;
+
+    const locationData = getLocationData();
+    const currentLocation = locationData.location;
+    const newLocation = locationData.isTestKontur ? currentLocation.replace(testKontur, kontur) : currentLocation.replace(kontur, testKontur);
+    window.location.href = newLocation;
 });
 
 document.body.appendChild(button);
+
