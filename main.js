24bf6749a16e3813b693736b4aad0a461abe821a
Denis Petukhov <d.petukhov@skbkontur.ru>
2024-04-12 21:12:46 +0300

    Add text color and remove magic strings

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const button = document.createElement('button');
+const kontur = ".kontur.ru";
+const testKontur = ".testkontur.ru";
+const toArrow = "→";
+const toKontur = toArrow + kontur;
+const toTestKontur = toArrow + testKontur;
+
 const currentUrl = window.location.toString();
-const isTestKontur = currentUrl.includes(".testkontur.ru");
-button.textContent = isTestKontur ? "→ kontur.ru" : "→ testkontur.ru";
-button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none"
+const button = document.createElement("button");
+const isTestKontur = currentUrl.includes(testKontur);
+button.textContent = isTestKontur ? toKontur : toTestKontur;
+button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none;color:rgb(102, 204, 255)"
 document.body.appendChild(button);
-button.addEventListener('click', () => {
-    const newUrl = isTestKontur ? currentUrl.replace(".testkontur.ru", ".kontur.ru") : currentUrl.replace(".kontur.ru", ".testkontur.ru");
+
+button.addEventListener('click', () => {    
+    const newUrl = isTestKontur ? currentUrl.replace(testKontur, kontur) : currentUrl.replace(kontur, testKontur);
     window.location.href = newUrl;
 });
