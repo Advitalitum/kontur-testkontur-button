1b7cae0e121ef7c01f125a078d306e759942d276
Denis Petukhov <d.petukhov@skbkontur.ru>
2024-04-14 14:36:23 +0300

    do not show button on kontur.ru or www.kontur.ru

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-const kontur = ".kontur.ru";
-const testKontur = ".testkontur.ru";
-const toArrow = "→ ";
-const toKontur = toArrow + kontur;
-const toTestKontur = toArrow + testKontur;
+const hostname = window.location.hostname;
 
-function getLocationData() {
-    const location = window.location.toString();
-    const isTestKontur = location.includes(testKontur);
-    return { location, isTestKontur };
-};
-
-const button = document.createElement("button");
-button.textContent = getLocationData().isTestKontur ? toKontur : toTestKontur;
-button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none;color:rgb(102, 204, 255)";
-
-button.addEventListener('click', () => {
-    const locationData = getLocationData();
-    const currentLocation = locationData.location;
-    const newLocation = locationData.isTestKontur ? currentLocation.replace(testKontur, kontur) : currentLocation.replace(kontur, testKontur);
-    window.location.href = newLocation;
-});
-
-document.body.appendChild(button);
+if (hostname != "kontur.ru" && hostname != "www.kontur.ru") {
+    const kontur = ".kontur.ru";
+    const testKontur = ".testkontur.ru";
+    const toArrow = "→ ";
+    const toKontur = toArrow + kontur;
+    const toTestKontur = toArrow + testKontur;
+    
+    function getLocationData() {
+        const location = window.location.toString();
+        const isTestKontur = location.includes(testKontur);
+        return { location, isTestKontur };
+    };
+    
+    
+    
+    const button = document.createElement("button");
+    button.textContent = getLocationData().isTestKontur ? toKontur : toTestKontur;
+    button.style = "top:0;right:0;position:fixed;z-index:9999;background:none;border:none;color:rgb(102, 204, 255)";
+    
+    button.addEventListener('click', () => {
+        const locationData = getLocationData();
+        const currentLocation = locationData.location;
+        const newLocation = locationData.isTestKontur ? currentLocation.replace(testKontur, kontur) : currentLocation.replace(kontur, testKontur);
+        window.location.href = newLocation;
+    });
+    
+    document.body.appendChild(button);
+}
